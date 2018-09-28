19c09f427ddfb815979bcec20dac67d60eed2d15
Marcelo Porcino <mporcino@thoughtworks.com>
2018-09-28 09:59:41 -0300

    adds ADD_TODO action

--- src/reducers/todo.test.js
+++ src/reducers/todo.test.js
@@ -1,11 +1,21 @@
-import todo from './todo';
+import todo, { Actions } from './todo';
 
 describe('todo', () => {
 
     it('returns the default state', () => {
         const state = todo();
 
-        expect(state).toEqual({});
-    })
+        expect(state).toEqual([]);
+    });
+
+    it ('adds a todo item', () => {
+        const state = todo([], { type: Actions.ADD_TODO, text: "Brush teeth" });
+
+        const expectedState = [
+            { text: "Brush teeth", completed: false }
+        ]
+
+        expect(state).toEqual(expectedState);
+    });
 
 });
