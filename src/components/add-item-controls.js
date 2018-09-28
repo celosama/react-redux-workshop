8f8408165553b3eb6bcd83b4592496cfbf8834a7
Marcelo Porcino <mporcino@thoughtworks.com>
2018-09-28 10:36:32 -0300

    adds dispatching of action to add a new item

--- src/components/add-item-controls.js
+++ src/components/add-item-controls.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Actions } from '../reducers/todo';
 
 export class AddItemControls extends Component {
+    constructor(props) {
+        super(props);
+        this.input = { value: "" }
+    }
+
+    addTodo(input) {
+        return {
+            type: Actions.ADD_TODO,
+            text: input
+        }
+    }
+
     render() {
+        const { dispatch } = this.props;
+        let text;
+
         return (
             <div>
-                <input type="text" id="item" name="item" placeholder="Type your todo item here" />
-                <button>Add Item</button>
+                <input ref={ input => text = input } type="text" id="item" name="item" placeholder="Type your todo item here" />
+                <button onClick={ () => { dispatch(this.addTodo(text.value)) } }>Add Item</button>
             </div>
         );
     }
